d5d243a61fb0dccb51bbca9288915c7dfc68d762
Robert Boyd <bert.vern.boyd@gmail.com>
2015-07-17 13:57:16 -0230

    added img radio buttons

--- A1/a1.js
+++ A1/a1.js
@@ -21,32 +21,21 @@ window.onload = function init()
     gl = WebGLUtils.setupWebGL( canvas );
     if ( !gl ) { alert( "WebGL isn't available" ); }
 
-    //
-    //  Configure WebGL
-    //
     gl.viewport( 0, 0, canvas.width, canvas.height );
     gl.clearColor( 1.0, 1.0, 1.0, 1.0 );
-
-    //  Load shaders and initialize attribute buffers
-
+    
     var program = initShaders( gl, "vertex-shader", "fragment-shader" );
     gl.useProgram( program );
 
-    // Load the data into the GPU
-
     var bufferId = gl.createBuffer();
     gl.bindBuffer( gl.ARRAY_BUFFER, bufferId );
-
-    // Associate out shader variables with our data buffer
-
+    
     var vPosition = gl.getAttribLocation( program, "vPosition" );
     gl.vertexAttribPointer( vPosition, 2, gl.FLOAT, false, 0, 0 );
     gl.enableVertexAttribArray( vPosition );
 
     thetaLoc = gl.getUniformLocation( program, "theta" );
     remesh = true;
-
-    // event handlers
     
     numTimesToSubdivide = document.getElementById("recursion_slider").value;
     document.getElementById("recursion_slider").onchange = function() {
