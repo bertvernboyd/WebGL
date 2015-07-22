6433b548bb43b9ca5f924f6b19eca2780d2c4b30
Robert Boyd <bert.vern.boyd@gmail.com>
2015-07-22 16:01:19 -0230

    multiple vertex buffers

--- A2/a2.js
+++ A2/a2.js
@@ -3,12 +3,15 @@
 var canvas;
 var jcanvas;
 var gl;
-var maxNumVertices  = 600;
+var maxNumVertices  = 60000;
 var index = 0;
 var paint = false;
 var color = vec4(0, 0, 0, 255);
 var endpoints = [0];
-
+var vPosition;
+var cBuffer;
+var vBuffers;
+var nBuffers = 5;
 
 window.onload = function init() {
   
@@ -31,19 +34,33 @@ window.onload = function init() {
   jcanvas.mouseup(function(event) {
       paint = false; 
   });
-
-  gl = WebGLUtils.setupWebGL( canvas );
-  if ( !gl ) { alert( "WebGL isn't available" ); }
   
   jcanvas.mousemove(function(event){
     if(paint){
-      gl.bindBuffer( gl.ARRAY_BUFFER, vBuffer );
-      var t = vec2(2*event.clientX/canvas.width-1,
-                   2*(canvas.height-event.clientY)/canvas.height-1);
+   
+      var t;
+
+      gl.bindBuffer( gl.ARRAY_BUFFER, vBuffers[0] );
+      t = c_to_s(event.clientX, event.clientY, canvas.width, canvas.height);
       gl.bufferSubData(gl.ARRAY_BUFFER, 8*index, flatten(t));
 
+      gl.bindBuffer( gl.ARRAY_BUFFER, vBuffers[1]);
+      t = c_to_s(event.clientX+1, event.clientY, canvas.width, canvas.height);
+      gl.bufferSubData(gl.ARRAY_BUFFER, 8*index, flatten(t));
+
+      gl.bindBuffer( gl.ARRAY_BUFFER, vBuffers[2]);
+      t = c_to_s(event.clientX-1, event.clientY, canvas.width, canvas.height);
+      gl.bufferSubData(gl.ARRAY_BUFFER, 8*index, flatten(t));
+
+      gl.bindBuffer( gl.ARRAY_BUFFER, vBuffers[3]);
+      t = c_to_s(event.clientX, event.clientY+1, canvas.width, canvas.height);
+      gl.bufferSubData(gl.ARRAY_BUFFER, 8*index, flatten(t));
+
+      gl.bindBuffer( gl.ARRAY_BUFFER, vBuffers[4]);
+      t = c_to_s(event.clientX, event.clientY-1, canvas.width, canvas.height);
+      gl.bufferSubData(gl.ARRAY_BUFFER, 8*index, flatten(t));
+      
       gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
-      t = vec4(0.0, 0.0, 0.0, 1.0);
       gl.bufferSubData(gl.ARRAY_BUFFER, 16*index, flatten(color));
       endpoints[endpoints.length-1] = ++index;
     }
@@ -62,8 +79,9 @@ window.onload = function init() {
       var cursorVal = "url(assets/" + cursorId + ".png), auto";
       $(".board").css("cursor", cursorVal);
   });
-  
-  
+
+  gl = WebGLUtils.setupWebGL( canvas );
+  if ( !gl ) { alert( "WebGL isn't available" ); }
 
     gl.viewport( 0, 0, canvas.width, canvas.height );    
     gl.clearColor( 1.0, 1.0, 1.0, 1.0 );
@@ -76,34 +94,46 @@ window.onload = function init() {
     var program = initShaders( gl, "vertex-shader", "fragment-shader" );
     gl.useProgram( program );
 
+    vBuffers = [];
 
-    var vBuffer = gl.createBuffer();
-    gl.bindBuffer( gl.ARRAY_BUFFER, vBuffer);
-    gl.bufferData( gl.ARRAY_BUFFER, 8*maxNumVertices, gl.STATIC_DRAW );
-
-    var vPosition = gl.getAttribLocation(program, "vPosition");
+    for(var v = 0; v < nBuffers; v++){
+      vBuffers[v] = gl.createBuffer();
+      gl.bindBuffer( gl.ARRAY_BUFFER, vBuffers[v]);
+      gl.bufferData( gl.ARRAY_BUFFER, 8*maxNumVertices, gl.STATIC_DRAW );
+    }
+    
+    vPosition = gl.getAttribLocation(program, "vPosition");
     gl.vertexAttribPointer(vPosition, 2, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(vPosition);
 
-    var cBuffer = gl.createBuffer();
+    cBuffer = gl.createBuffer();
     gl.bindBuffer( gl.ARRAY_BUFFER, cBuffer );
     gl.bufferData( gl.ARRAY_BUFFER, 16*maxNumVertices, gl.STATIC_DRAW );
 
     var vColor = gl.getAttribLocation( program, "vColor" );
     gl.vertexAttribPointer( vColor, 4, gl.FLOAT, false, 0, 0 );
     gl.enableVertexAttribArray( vColor );
-
+    
     render();
+}
+
+function c_to_s(x, y, w, h){
+    return vec2(2*x/w-1, 2*(h-y)/h-1);
 }
 
 
 function render() {
     gl.clear( gl.COLOR_BUFFER_BIT );
-    
-    for(var i = 0; i < endpoints.length; i++){
+
+    for(var v = 0; v < vBuffers.length; v++){
+      gl.bindBuffer( gl.ARRAY_BUFFER, vBuffers[v]);
+      gl.vertexAttribPointer(vPosition, 2, gl.FLOAT, false, 0, 0);
+
+      for(var i = 0; i < endpoints.length; i++){
         var a = (i == 0) ? 0 : endpoints[i-1];
         var b = endpoints[i];   
         gl.drawArrays( gl.LINE_STRIP, a, b-a );        
+      }
     }
 
     window.requestAnimFrame(render);
